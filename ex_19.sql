32cf55be88b1a4deff07d034181a782b551e3fde
doommc8q <103940424+doommc8q@users.noreply.github.com>
2022-05-16 22:31:39 +0600

    add 19.sql

---
+++ ex_19.sql
@@ -0,0 +1,9 @@
+SELECT Sum(I.Total) TotalSumIn2009, e.FirstName || ' ' || e.LastName AS SalesAgent
+FROM Invoice I
+         INNER JOIN Customer c ON i.CustomerId = c.CustomerId
+         INNER JOIN Employee E on c.SupportRepId = E.EmployeeId
+WHERE strftime('%Y', InvoiceDate) = '2010' and Title like '%sales%'
+   or '%agent%'
+GROUP BY e.FirstName
+ORDER BY Sum(I.Total) DESC
+LIMIT 1;

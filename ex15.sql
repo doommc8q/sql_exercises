f02243c8e3a11529da5d9560d4a5812ee75db147
doommc8q <103940424+doommc8q@users.noreply.github.com>
2022-05-16 22:44:16 +0600

    Delete ex15.sql

--- ex15.sql
+++
@@ -1,5 +0,0 @@
-SELECT T.Name AS TrackName, A.Title AS AlbumTitle, M.Name AS MediaTypeName, G.Name AS GenreName
-FROM Track T
-         INNER JOIN Album A on T.AlbumId = A.AlbumId
-         INNER JOIN Genre G on T.GenreId = G.GenreId
-         INNER JOIN MediaType M on T.MediaTypeId = M.MediaTypeId;
